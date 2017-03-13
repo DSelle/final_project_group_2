f0ceca196ad1f5b47498bb4e91f7b690242e7d05
Nicholas Perez <nperez9@mail.greenriver.edu>
2017-03-13 14:32:06 -0500

    Removed tmp folder from git

--- tmp/23vwopqj6bhco.1s33lx129wxjx.php
+++
@@ -1,8 +0,0 @@
-<p>Home Page</p>
-
-<ul>
-    <li><a href="<?= $BASE; ?>">HOME</a></li>
-    <li><a href="<?= $BASE; ?>/menu">MENU</a></li>
-    <li><a href="<?= $BASE; ?>/receipt">RECEIPT</a></li>
-    <li><a href="<?= $BASE; ?>/cart">CART</a></li>
-</ul>
