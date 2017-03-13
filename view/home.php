e75e9d851ebae1204e62436a4af07bb184bafd91
Nicholas Perez <nperez9@mail.greenriver.edu>
2017-03-13 16:13:58 -0500

    Finished html and css work for hte home page. The css will be used across all view files. Also added an images folder and a place holder image for hte menu options

--- view/home.php
+++ view/home.php
@@ -1,51 +1,46 @@
-<div class="container">
+<div class="container ">
+    
     <div class="row">
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
+        <br><br><br><br>
+
+        <div class="col-sm-12 box_body text-center">
+            <h1><strong>[</strong> Trendy American Food Place <strong>]</strong></h1>
+            <p>Est. 1437 B.C as a true example of American Cuisine</p>
+            <br>
+        </div>
+        <br><br><br><br><br><br><br><br>
     </div>
     
     <div class="row">
-        <div class="col-sm-offset-2 col-sm-2 box_body text-center">
-            <h1>Menu</h1>
-        </div>
         
-        <div class="col-sm-offset-1 col-sm-2 col-sm-offset-1 box_body text-center">
-            <h1>Receipt</h1>
-        </div>
+        <a href="{{@BASE}}/menu">
+            <div class="col-sm-offset-2 col-sm-2 box_body text-center">
+                <h3><strong>[ Menu ]</strong></h3>
+                <br>
+            </div>
+        </a>
+        </strong>
+        <a href="{{@BASE}}/receipt">
+            <div class="col-sm-offset-1 col-sm-2 col-sm-offset-1 box_body text-center">
+                <h3><strong>[ Receipt ]</strong></h3>
+                <br>
+            </div>
+        </a>
         
-        <div class="col-sm-2 col-sm-offset-1 box_body text-center">
-            <h1>Cart</h1>
+        <a href="{{@BASE}}/cart">
+            <div class="col-sm-2 col-sm-offset-1 box_body text-center">
+                <h3><strong>[ Cart ]</strong></h3>
+                <br>
+            </div>
+        </a>
+        
+    </div>
+    
+    <div class="row">
+        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+        <div class="col-sm-12 sub_text text-center fixed-bottom">
+        <p>© 2017 Trendy American Food Place Restaurants LLC.</p>
+        <p>The Trendy American Food Place’s logo is a registered trademark and copyrighted work of Trendy American Food Place Restaurants LLC.</p>
         </div>
-    </div>   
+    </div>
 </div>
-
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-
-<p>Home Page</p>
-
-<ul>
-    <li><a href="{{@BASE}}">HOME</a></li>
-    <li><a href="{{@BASE}}/menu">MENU</a></li>
-    <li><a href="{{@BASE}}/receipt">RECEIPT</a></li>
-    <li><a href="{{@BASE}}/cart">CART</a></li>
-</ul>
