dd87ed15297bba131714a9a9b800c39fa76c0ea2
Derrick Selle <dselle4@mail.greenriver.edu>
2017-03-13 16:10:10 -0500

    Added database connection file.

--- controller/order_controller.php
+++ controller/order_controller.php
@@ -1,28 +1,34 @@
 <?php
+  require_once 'model/menu_db.php';
+
     class OrderController
     {
         private $_f3; //router
-         
+
         public function __construct($f3)
         {
             $this->_f3 = $f3;
         }
-        
+
         public function home()
         {
             echo Template::instance()->render('view/home.php');
         }
-        
+
         public function menu()
         {
+            $test = getMenu();
+            //echo '<h1>Test: '.$test.'</h1>';
+            var_dump($test);
+            exit();
             echo Template::instance()->render('view/menu.php');
         }
-        
+
         public function receipt()
         {
             echo Template::instance()->render('view/receipt.php');
         }
-        
+
         public function cart()
         {
             echo Template::instance()->render('view/cart.php');
--- model/menu_db.php
+++ model/menu_db.php
@@ -1,3 +1,15 @@
 <?php
+require_once 'connection.php';
 
+function getMenu()
+{
+  $pdo = getConnection();
+
+  $select = 'SELECT food_name, price, image_path FROM menu';
+  $results = $pdo->query($select);
+
+  $rows = $results->fetchAll(PDO::FETCH_ASSOC);
+
+  return $rows;
+}
 ?>
