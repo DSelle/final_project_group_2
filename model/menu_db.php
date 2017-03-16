86a658b4e6fcff83dff300e67644da23bb3c0728
Derrick Selle <dselle4@mail.greenriver.edu>
2017-03-16 13:21:50 -0500

    Debugged receipt database functions.

--- model/menu_db.php
+++ model/menu_db.php
@@ -35,37 +35,4 @@ function getTypeMenu($category)
 
   return $rows;
 }
-
-function getAllReceipt()
-{
-  $pdo = getConnection();
-
-  $select = 'SELECT id, table_num, menu_item, item_quantity,
-  sub_total, tax, tip, total
-  FROM receipt';
-  $results = $pdo->query($select);
-
-  $rows = $results->fetchAll(PDO::FETCH_ASSOC);
-
-  return $rows;
-}
-
-function getReceipt($id)
-{
-  $pdo = getConnection();
-
-  $select = 'SELECT table_num, menu_item, item_quantity,
-  sub_total, tax, tip, total
-  FROM receipt
-  WHERE id=:id';
-
-  $statement = $pdo->prepare($select);
-
-  $statement->bindValue(':id', $id, PDO::PARAM_STR);
-  $statement->execute();
-
-  $row = $statement->fetch(PDO::FETCH_ASSOC);
-
-  return $rows;
-}
 ?>
--- model/receipt_db.php
+++ model/receipt_db.php
@@ -1,3 +1,36 @@
 <?php
+require_once 'connection.php';
 
+function getAllReceipt()
+{
+  $pdo = getConnection();
+
+  $select = 'SELECT id, table_num, menu_item, item_quantity,
+  sub_total, tax, tip, total
+  FROM receipt';
+  $results = $pdo->query($select);
+
+  $rows = $results->fetchAll(PDO::FETCH_ASSOC);
+
+  return $rows;
+}
+
+function getReceipt($id)
+{
+  $pdo = getConnection();
+
+  $select = 'SELECT table_num, menu_item, item_quantity,
+  sub_total, tax, tip, total
+  FROM receipt
+  WHERE id=:id';
+
+  $statement = $pdo->prepare($select);
+
+  $statement->bindValue(':id', $id, PDO::PARAM_STR);
+  $statement->execute();
+
+  $rows = $statement->fetch(PDO::FETCH_ASSOC);
+
+  return $rows;
+}
 ?>
